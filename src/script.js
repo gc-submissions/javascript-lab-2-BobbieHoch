9b74d0d61389791c6070af582a1cf22cd0f077ca
BobbieHoch <rwhochstadt@gmail.com>
2022-05-02 22:01:27 -0400

    Not sure what file is being updated

--- src/script.js
+++ src/script.js
@@ -1,3 +1,31 @@
 "use strict";
 
 // TODO - write your code here.
+//
+const randomDamage = () =>{
+    return(Math.floor((Math.random() * 10) + 1)
+}
+
+const chooseOption =(opt1, opt2) {
+    let randNum = Math.floor(Math.random() * 11) => {
+    return (randNum) 
+    else return opt1:opt2;
+}
+
+function attackPlayer= (health-randomDamage)
+
+function logHealth (player, health)
+    console.log(`${player}+ logHealth + health`)
+    return {health<=0)
+}
+function isDead(health)
+
+function fight (player1,player2, player1Health, player2health)
+    while(attacker==chooseOption(player1,))
+
+const attacker = chooseOption(player1, player2);
+    if(attacker == player1){player2Health=attackPlayer}
+
+    }
+
+}
