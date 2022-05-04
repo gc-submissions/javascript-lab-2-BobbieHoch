b341574f30594e3a64aafef674dc2be0442001a8
BobbieHoch <rwhochstadt@gmail.com>
2022-05-04 00:26:50 -0400

    call function??

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 
 // TODO - write your code here.
 //
@@ -7,18 +7,18 @@ const randomDamage = () =>{
 }
 
 const chooseOption =(opt1, opt2) {
-    let randNum = Math.floor(Math.random() * 11) => {
-    return (randNum) 
-    else return opt1:opt2;
+    let randNum = Math.floor(Math.random() * 1) => {
+    return randNum === 0 ? opt1 : opt2;
 }
 
-function attackPlayer= (health-randomDamage)
-
-function logHealth (player, health)
-    console.log(`${player}+ logHealth + health`)
-    return {health<=0)
+function attackPlayer(health) {
+    return (health-randomDamage)
 }
-function isDead(health)
+function logHealth = (player, health) => (`${player}+ logHealth +":" health`);
+    console.log(`${player}+ logHealth +":" health`)
+}
+// don't know how to get a boolean function out of an Arrow function
+function isDead(health)=> health <= 0
 
 function fight (player1,player2, player1Health, player2health)
     while(attacker==chooseOption(player1,))
@@ -27,5 +27,16 @@ const attacker = chooseOption(player1, player2);
     if(attacker == player1){player2Health=attackPlayer}
 
     }
+//     Set player2Health equal to the result of attackPlayer with player2Health as its argument.
+// Calls the logHealth function with player2 and player2Health as its arguments.
+// If the result of isDead with player2Health as an argument is true:
+// Call the logDeath function with player1 and player2 as arguments.
+// Break
+player2Health=attachPlayer(player2Health)
+if(player2Health)
+
+// Has an else statement that:
+// Sets player1Health equal to the attackPlayer function with player1Health as its argument.
+
 
 }
