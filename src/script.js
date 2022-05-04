083dbab89821d93f2b56f606fff1f9ba1d4e8aa2
BobbieHoch <rwhochstadt@gmail.com>
2022-05-04 19:39:34 -0400

    After Class Review

--- src/script.js
+++ src/script.js
@@ -2,41 +2,62 @@
 
 // TODO - write your code here.
 //
-const randomDamage = () =>{
+const randomDamage = () => {
     return(Math.floor((Math.random() * 10) + 1)
-}
+};
 
-const chooseOption =(opt1, opt2) {
-    let randNum = Math.floor(Math.random() * 1) => {
+const chooseOption =(opt1, opt2) => {
+    const randNum = Maath.floor(Math.random()* 2);
+    // let randNum = Math.floor(Math.random() * 1) => {
     return randNum === 0 ? opt1 : opt2;
-}
+};
+const attackPlayer = function(health) {
+    return health-randomDamage();
+};
 
-function attackPlayer(health) {
-    return (health-randomDamage)
-}
-function logHealth = (player, health) => (`${player}+ logHealth +":" health`);
-    console.log(`${player}+ logHealth +":" health`)
-}
+const logHealth = (player, health) => {
+    console.log(`${player}+ health:" health`)
+};
+//template string-string interpolation
 // don't know how to get a boolean function out of an Arrow function
-function isDead(health)=> health <= 0
+const logDeath = (winner, loser) => {
+    console.log (`${winner} defeated ${loser}`);
+};
 
-function fight (player1,player2, player1Health, player2health)
-    while(attacker==chooseOption(player1,))
+function isDead(health) => {
+    return health <= 0 ? true : false;
+};
 
-const attacker = chooseOption(player1, player2);
-    if(attacker == player1){player2Health=attackPlayer}
+function fight (player1, player2, player1Health, player2Health) {
+    while(true){
+        const attacker = chooseOption(player1, player2);
 
+    if(attacker === player1) {
+        player2Health = attackPlayer(player2Health);
+        logHealth(player2, player2Health);
+
+    if(isDead(player2Health)){
+            logDeath(player2, player1);
+            break;
     }
+    else{
+             player1Health = attackPlayer(player1Health);
+             logHealth(player1, player1Helath);
+
+    if (isDead(player1Health)){
+        logDeath(player2, player1);
+        break;
+         }
+        }
+    }
+}
+
+ fight('Auriel', 'Mitch', 100,100);
 //     Set player2Health equal to the result of attackPlayer with player2Health as its argument.
 // Calls the logHealth function with player2 and player2Health as its arguments.
 // If the result of isDead with player2Health as an argument is true:
 // Call the logDeath function with player1 and player2 as arguments.
 // Break
-player2Health=attachPlayer(player2Health)
-if(player2Health)
-
 // Has an else statement that:
 // Sets player1Health equal to the attackPlayer function with player1Health as its argument.
 
-
-}
